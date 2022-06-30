816bd9867a1cf681fd8485a06b73059519384c14
odininon <mike.stengel@gmail.com>
2022-06-29 22:35:19 -0700

    Fix message capture

--- config/runtime.exs
+++ config/runtime.exs
@@ -2,4 +2,5 @@ import Config
 
 config :nostrum,
   token: System.get_env("DISCORD_TOKEN"),
-  num_shards: :auto
+  num_shards: :auto,
+  gateway_intents: [:message_content, :guilds, :guild_messages]
